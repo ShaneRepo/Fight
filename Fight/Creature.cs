0f3bb153814f1aa849cf713080c1377c13b95a5e
Shane <shane8641@live.com>
2015-12-10 18:22:39 -0500

    added Creature base class

---
+++ Fight/Creature.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fight
+{
+    class Creature
+    {
+        private int _strength;
+        private int _hitpoints;
+        private string _name;
+        private int _damage;
+
+        public Creature()
+        {
+            _name = "Base Creature";
+        }
+        public Creature(int strength, int hitpoints)
+        {
+            _strength = strength;
+            _hitpoints = hitpoints;
+        }
+        public int Strength
+        {
+            get { return _strength; }
+            set { _strength = value; }
+        }
+        public int Hitpoints
+        {
+            get { return _hitpoints; }
+            set { _hitpoints = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public virtual int Damage
+        {
+            get { return _damage; }
+            set { _damage = value; }   
+        }
+
+    }
+}
